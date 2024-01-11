d273937ed45c152242f72ce863716c1a3540d9fd
Michael Abel <info@abel-ikt.de>
2024-01-11 16:52:55 +0100

    Add test project for the beep

---
+++ demos/beep/src/main.rs
@@ -0,0 +1,25 @@
+#![no_std]
+#![no_main]
+
+use panic_rtt_target as _;
+use rtt_target::{rtt_init_print, rprintln};
+use cortex_m_rt::entry;
+
+use calliope::Board;
+use calliope::beep;
+
+use calliope::hal::timer::Timer;
+use calliope::hal::pac::TIMER0;
+
+#[entry]
+fn main() -> ! {
+    rtt_init_print!();
+    rprintln!("Hello World");
+
+    let mut board = Board::take().unwrap();
+    let mut timer = Timer::new(board.TIMER0);
+
+    beep(&mut timer);
+
+    loop {}
+}
