bdce07c275179cb17f62ea56925979e91a0edfae
Michael Abel <info@abel-ikt.de>
2024-01-11 20:26:56 +0100

    Add animation

--- demos/ws2812/src/main.rs
+++ demos/ws2812/src/main.rs
@@ -6,18 +6,118 @@ use rtt_target::{rtt_init_print, rprintln};
 
 use cortex_m_rt::entry;
 
+use calliope::Board;
 use calliope::set_ws2812;
+use calliope::hal::timer::Timer;
+use calliope::hal::prelude::_embedded_hal_blocking_delay_DelayMs;
+use calliope::hal::timer::Instance;
+
+enum LedState {
+    GreenToYellow,
+    YellowToRed,
+    RedToPink,
+    PinkToBlue,
+    BlueToCyan,
+    CyanToGreen,
+}
+
+
+fn animation <T :Instance, U>( timer : &mut Timer<T,U>) -> ! {
+
+    let mut r:u8 = 0;
+    let mut g:u8 = 0;
+    let mut b:u8 = 0;
+
+    let max = 0x20;
+    let min = 0x00;
+
+    let d = 50u16;
+
+    let value =0;
+    let mut state: LedState = LedState::GreenToYellow;
+
+    g = max;
+
+    loop{
+
+    match state {
+        LedState::GreenToYellow =>
+            {
+                r=r+1;
+                if r >= max {
+                    state=LedState::YellowToRed;
+                }
+            },
+        LedState::YellowToRed =>
+            {
+                g=g-1;
+                if g <= min {
+                    state=LedState::RedToPink;
+                }
+            },
+        LedState::RedToPink =>
+            {
+                b=b+1;
+                if b >= max {
+                    state=LedState::PinkToBlue;
+                }
+            },
+        LedState::PinkToBlue =>
+            {
+                r=r-1;
+                if r <= min {
+                    state=LedState::BlueToCyan;
+                }
+            },
+        LedState::BlueToCyan =>
+            {
+                g=g+1;
+                if g >= max {
+                    state=LedState::CyanToGreen;
+                }
+            },
+        LedState::CyanToGreen =>
+            {
+                b=b-1;
+                if b <= min {
+                    state=LedState::GreenToYellow;
+                }
+            },
+        }
+    set_ws2812(r,g,b);
+    timer.delay_ms(d);
+    }
+}
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
+    let board = Board::take().unwrap();
+    let mut timer = Timer::new(board.TIMER0);
+
     rprintln!("Hello World");
+
+    let delay = 1000u32;
 
     let mut r:u8 = 0x20;
     let mut g:u8 = 0x00;
     let mut b:u8 = 0x20;
     
+    r=0x20; g=0x00; b=0x00;
     set_ws2812(r,g,b);
+    timer.delay_ms(delay);
 
-    loop {}
+    r=0x00; g=0x20; b=0x00;
+    set_ws2812(r,g,b);
+    timer.delay_ms(delay);
+
+    r=0x00; g=0x00; b=0x20;
+    set_ws2812(r,g,b);
+    timer.delay_ms(delay);
+
+    r=0x00; g=0x00; b=0x00;
+    set_ws2812(r,g,b);
+    timer.delay_ms(delay);
+
+    animation(&mut timer);
 }
