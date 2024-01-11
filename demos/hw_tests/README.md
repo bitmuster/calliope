201fc2def2b956ea3c0d57d9b7366c1a6f82f08c
Michael Abel <info@abel-ikt.de>
2024-01-11 21:14:08 +0100

    Add test project

---
+++ demos/hw_tests/README.md
@@ -0,0 +1,12 @@
+
+Basic 1s blink without RTT attached (Segger Real Time Transfer ?)
+
+Compile and run:
+
+    cargo embed --target thumbv6m-none-eabi
+
+
+This demo is based on the microbit setup example from the rust
+discovery book:
+
+https://github.com/rust-embedded/discovery/tree/master/microbit/src/03-setup
---
+++ demos/hw_tests/src/main.rs
@@ -0,0 +1,56 @@
+#![no_std]
+#![no_main]
+
+use panic_rtt_target as _;
+//use panic_halt as _;
+use rtt_target::{rtt_init_print, rprintln};
+
+use cortex_m_rt::entry;
+
+use calliope::Board;
+
+use calliope::hal::timer::Timer;
+use calliope::hal::prelude::_embedded_hal_blocking_delay_DelayMs;
+use calliope::hal::prelude::OutputPin;
+use calliope::{pin4_set_high_outset, pin4_set_high_gpio, pin4_set_high_asm};
+
+#[entry]
+fn main() -> ! {
+    rtt_init_print!();
+
+    let board = Board::take().unwrap();
+    let mut timer = Timer::new(board.TIMER0);
+
+    let delay = 1000u32;
+    let mut col1 = board.display_pins.col1;
+    let mut row1 = board.display_pins.row1;
+
+    row1.set_high().unwrap();
+
+    loop {
+        rprintln!("Hello set_high");
+
+        col1.set_high().unwrap();
+        timer.delay_ms(delay);
+        col1.set_low().unwrap();
+        timer.delay_ms(delay);
+        
+        rprintln!("Hello outset");
+        pin4_set_high_outset();
+        timer.delay_ms(delay);
+        col1.set_low().unwrap();
+        timer.delay_ms(delay);
+
+        rprintln!("Hello gpio");
+        pin4_set_high_gpio();
+        timer.delay_ms(delay);
+        col1.set_low().unwrap();
+        timer.delay_ms(delay);
+        
+        rprintln!("Hello asm");
+        pin4_set_high_asm();
+        timer.delay_ms(delay);
+        col1.set_low().unwrap();
+        timer.delay_ms(delay);
+    }
+}
